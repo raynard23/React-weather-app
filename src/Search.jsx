f0c164daca0d41928e59765aaf4f32d630c711f2
Raynard <Raynardchaney@gmail.com>
2022-04-11 10:06:00 -0400

    search bar done

---
+++ src/Search.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+
+const Search = () => {
+    return (
+        <div>
+            <input
+                type="search"
+                placeholder="Search a city"
+            />
+        <button>enter</button> 
+        </div>
+       
+    )
+}
+
+
+export default Search;
